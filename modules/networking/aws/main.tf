9902336c53bd59477ed39f06b1dd636be63fa2ff
Vegetam <francesco.malagrino@gmail.com>
2026-02-23 15:56:13 +0100

    fix network amazon

--- modules/networking/aws/main.tf
+++ modules/networking/aws/main.tf
@@ -7,8 +7,14 @@ variable "vpc_cidr" { type = string }
 variable "availability_zones" { type = list(string) }
 variable "public_subnets" { type = list(string) }
 variable "private_subnets" { type = list(string) }
-variable "enable_nat_gateway" { type = bool; default = true }
-variable "single_nat_gateway" { type = bool; default = false }
+variable "enable_nat_gateway" {
+  type    = bool
+  default = true
+}
+variable "single_nat_gateway" {
+  type    = bool
+  default = false
+}
 
 locals {
   name_prefix = "${var.project_name}-${var.environment}"
